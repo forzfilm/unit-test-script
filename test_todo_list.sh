bd6ac9b24e6279f5ac75eb82741ca8bb538c3102
film.l <pattaravadee.l@ibmdt.co.th>
2025-12-18 16:22:36 +0700

    Check existing folder and remove before run test

--- test_todo_list.sh
+++ test_todo_list.sh
@@ -1,3 +1,21 @@
+BUILD_PATH="build"
+
+if [ -d "$BUILD_PATH" ]; then
+  echo "BUILD_PATH exists. Removing $BUILD_PATH"
+  rm -rf "$BUILD_PATH"
+else
+  echo "BUILD_PATH does not exist. Nothing to remove."
+fi
+
+XCOV_PATH="xcov_report"
+
+if [ -d "$XCOV_PATH" ]; then
+  echo "XCOV_PATH exists. Removing $XCOV_PATH"
+  rm -rf "$XCOV_PATH"
+else
+  echo "XCOV_PATH does not exist. Nothing to remove."
+fi
+
 xcodebuild test \
   -workspace Example1.xcworkspace \
   -scheme "ToDoList_Example" \
